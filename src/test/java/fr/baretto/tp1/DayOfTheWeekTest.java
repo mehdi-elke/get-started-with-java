3dae37ec8c6145438ae99cd571ae11883fee0175
Sacha Duvivier <sduvivier@walibuy.com>
2024-10-17 16:30:22 +0200

    Melene Dufrenois/Maxime Masson/Sacha Duvivier

--- src/test/java/fr/baretto/tp1/DayOfTheWeekTest.java
+++ src/test/java/fr/baretto/tp1/DayOfTheWeekTest.java
@@ -21,6 +21,15 @@ public class DayOfTheWeekTest {
     }
 
     public String getDay(final int value) {
-        return null;
+        return switch (value) {
+            case 1 -> "monday";
+            case 2 -> "tuesday";
+            case 3 -> "wednesday";
+            case 4 -> "thursday";
+            case 5 -> "friday";
+            case 6 -> "saturday";
+            case 7 -> "sunday";
+            default -> "error";
+        };
     }
 }
