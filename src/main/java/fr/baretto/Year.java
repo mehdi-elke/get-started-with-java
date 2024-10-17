f1817827eb82da41360c2a3bf710e88fc8f192c2
theodadon <theo.td49@gmail.com>
2024-10-17 16:21:45 +0200

    Ex 6 mehdi_theo_yves

--- src/main/java/fr/baretto/Year.java
+++ src/main/java/fr/baretto/Year.java
@@ -3,6 +3,6 @@ package fr.baretto;
 public class Year {
 
     public static boolean isLeapYear(final int value) {
-        return false;
+        return (value % 400 == 0) || (value % 100 != 0 && value % 4 == 0);
     }
 }
