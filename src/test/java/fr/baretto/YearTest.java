f40abe4f3f644a1243520503fd149c703c8c9f7f
Sacha Duvivier <sduvivier@walibuy.com>
2024-10-17 16:30:22 +0200

    Melene Dufrenois/Maxime Masson/Sacha Duvivier

--- src/test/java/fr/baretto/YearTest.java
+++ src/test/java/fr/baretto/YearTest.java
@@ -8,11 +8,15 @@ class YearTest {
     @Test
     public void isALeapYear() {
         Assertions.assertTrue(Year.isLeapYear(2000));
+        Assertions.assertTrue(Year.isLeapYear(2008));
+
     }
 
     @Test
     public void isNotALeapYear() {
         Assertions.assertFalse(Year.isLeapYear(1900));
+        Assertions.assertFalse(Year.isLeapYear(2100));
+        Assertions.assertFalse(Year.isLeapYear(2018));
     }
 
 }
