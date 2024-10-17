bc18ba0eac3b9dfa71d241c18f7f218649eb7d66
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2024-10-17 16:49:47 +0200

    tjrs tp
    tjrs tp

--- src/main/java/fr/baretto/Book.java
+++ src/main/java/fr/baretto/Book.java
@@ -6,4 +6,10 @@ public class Book {
     int yearPublished;
     double price;
 
+    public Book(String title, String author, int yearPublished, double price) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.price = price;
+    }
 }
