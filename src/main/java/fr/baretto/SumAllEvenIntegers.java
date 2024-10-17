e8290ef95d6c667611f0c242ca30e4993b251f7c
Sacha Duvivier <sduvivier@walibuy.com>
2024-10-17 15:52:28 +0200

    Melene Dufrenois/Maxime Masson/Sacha Duvivier

---
+++ src/main/java/fr/baretto/SumAllEvenIntegers.java
@@ -0,0 +1,23 @@
+package fr.baretto;
+
+import java.util.Scanner;
+
+public class SumAllEvenIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+        int sum = 0;
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                sum += i;
+            }
+            System.out.println("The sum of all integers from 1 to " + number + " is: " + sum);
+        } else {
+            System.out.println("The sum of all integers from 1 to " + number + " is: " + sum);
+        }
+
+        scanner.close();
+    }
+}
