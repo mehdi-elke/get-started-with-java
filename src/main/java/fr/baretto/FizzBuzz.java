898f9aafd588ca3970065aaa9214a48a820e8a36
Adrien Degand <adrien.degand@hotmail.com>
2024-10-17 16:04:06 +0200

    Add FizzBuzz

--- src/main/java/fr/baretto/FizzBuzz.java
+++ src/main/java/fr/baretto/FizzBuzz.java
@@ -3,6 +3,14 @@ package fr.baretto;
 public class FizzBuzz {
 
     public static String get(final int value) {
-        return null;
+        if (value % 3 == 0 && value % 5 == 0) {
+            return "FizzBuzz";
+        } else if (value % 3 == 0) {
+            return "Fizz";
+        } else if (value % 5 == 0) {
+            return "Buzz";
+        } else {
+            return String.valueOf(value);
+        }
     }
 }
