a9433ada3398f1c5d3bd34f5c1a3b205e2d79270
Sacha Duvivier <sduvivier@walibuy.com>
2024-10-17 16:31:47 +0200

    Melene Dufrenois/Maxime Masson/Sacha Duvivier

--- src/main/java/fr/baretto/tp1/FizzBuzz.java
+++ src/main/java/fr/baretto/tp1/FizzBuzz.java
@@ -1,8 +1,18 @@
 package fr.baretto.tp1;
 
+import java.util.List;
+
 public class FizzBuzz {
 
     public static String get(final int value) {
-        return null;
+        if (value % 15 == 0){ // ou 5 == 0 && value % 3
+            return "FizzBuzz";
+        } else if (value % 5 ==0){
+            return "Buzz";
+        } else if (value % 3 ==0){
+            return "Fizz";
+        }else{
+            return String.valueOf(value);
+        }
     }
 }
