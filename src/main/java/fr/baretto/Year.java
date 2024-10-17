79bc304e3d3ba36e79c88637141184706fd5df0e
Adrien Degand <adrien.degand@hotmail.com>
2024-10-17 16:11:37 +0200

    Add LeapYear

--- src/main/java/fr/baretto/Year.java
+++ src/main/java/fr/baretto/Year.java
@@ -3,6 +3,13 @@ package fr.baretto;
 public class Year {
 
     public static boolean isLeapYear(final int value) {
+        if (value % 400 == 0) {
+            return true;
+        }
+        if (value % 4 == 0 && value % 100 != 0) {
+            return true;
+        }
+
         return false;
     }
 }
