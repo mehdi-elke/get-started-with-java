3bcbb2ba52c0ad186f524a81d3bbeabc9c8c0ad9
Kylian <libraaa201@gmail.com>
2024-10-22 15:30:56 +0200

    Application_WIP

--- src/main/java/fr/baretto/tp2/Application.java
+++ src/main/java/fr/baretto/tp2/Application.java
@@ -1,4 +1,11 @@
 package fr.baretto.tp2;
 
 public class Application {
+    public static void main(String[] args) {
+        Restaurant foodFast = new Restaurant();
+        DeliveryPlateform foodFastLivraison = new DeliveryPlateform();
+
+
+
+    }
 }
