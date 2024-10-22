60174215af713ff0cfba21d0519701aefcd00336
Adrien Degand <adrien.degand@hotmail.com>
2024-10-22 15:50:24 +0200

    fix delivery plateform

--- src/main/java/fr/baretto/tp2/DeliveryPlateform.java
+++ src/main/java/fr/baretto/tp2/DeliveryPlateform.java
@@ -7,6 +7,6 @@ public class DeliveryPlateform {
     void delivery(Order order) throws InterruptedException {
         System.out.println("Livraisons de la commande à " + order.getDeliveryPlace() + "...");
         Thread.sleep(new Random().nextInt(3000));
-        System.out.println("Commande livrée, vous payez " + order.getPrice() + "sivouplé.");
+        System.out.println("Commande livrée, vous devez payer " + order.getPrice() + " sivouplé.");
     }
 }
