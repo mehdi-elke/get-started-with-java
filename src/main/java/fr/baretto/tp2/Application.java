9f402489acbf1e21b0c344640d592241f4a118c7
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2024-10-22 16:17:03 +0200

    Add observer
    Add observer

--- src/main/java/fr/baretto/tp2/Application.java
+++ src/main/java/fr/baretto/tp2/Application.java
@@ -5,14 +5,14 @@ public class Application {
     public static void main(String[] args) throws InterruptedException {
         Restaurant restaurant = new Restaurant("FoodFast");
         DeliveryPlateform foodFastLivraison = new DeliveryPlateform();
+        restaurant.addObserver(foodFastLivraison);
         Dish pates = new Dish("pâtes", DishSize.M);
         Dish lasagnes = new Dish("lasagnes", DishSize.L);
         Dish pizza = new Dish("pizza", DishSize.S);
 
 
 
-        Order order = restaurant.prepareOrder(List.of(pates, lasagnes, pizza), 30, "Lille");
-        foodFastLivraison.delivery(order);
+        restaurant.prepareOrder(List.of(pates, lasagnes, pizza), 30, "Lille");
 
     }
 }
--- src/main/java/fr/baretto/tp2/DeliveryPlateform.java
+++ src/main/java/fr/baretto/tp2/DeliveryPlateform.java
@@ -2,11 +2,18 @@ package fr.baretto.tp2;
 
 import java.util.Random;
 
-public class DeliveryPlateform {
+public class DeliveryPlateform implements Observer{
 
     void delivery(Order order) throws InterruptedException {
         System.out.println("Livraisons de la commande à " + order.getDeliveryPlace() + "...");
         Thread.sleep(new Random().nextInt(3000));
         System.out.println("Commande livrée, vous devez payer " + order.getPrice() + " sivouplé.");
+    }
+
+    @Override
+    public void update(String event, Order order) throws InterruptedException {
+        if(event.equals("finished order")){
+            this.delivery(order);
+        }
     }
 }
