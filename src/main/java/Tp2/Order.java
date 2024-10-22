b8eea82325ffbdb7fa35388063d4afe16a016d0f
Alertinime <tinoufischer@gmail.com>
2024-10-22 15:08:12 +0200

    add Order class

--- src/main/java/Tp2/Order.java
+++ src/main/java/Tp2/Order.java
@@ -7,4 +7,11 @@ public class Order
     private int quantity;
     private int price;
     private String adresse;
+    public Order(Restaurant restaurant, Dish dish, int quantity, int price, String adresse){
+        this.resto = restaurant;
+        this.dish = dish;
+        this.quantity = quantity;
+        this.price = price;
+        this.adresse = adresse;
+    }
 }
