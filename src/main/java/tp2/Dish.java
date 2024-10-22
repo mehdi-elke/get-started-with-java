6dbd186742d1d7899629c796102506ffea5c44a8
Vianney Lehu <60597553+RicqPascalv2@users.noreply.github.com>
2024-10-22 15:24:02 +0200

    class dish

--- src/main/java/tp2/Dish.java
+++ src/main/java/tp2/Dish.java
@@ -1,4 +1,33 @@
 package tp2;
 
 public class Dish {
+    private String nom;
+    private enum Taille {
+        S,
+        M,
+        L
+    }
+    private Taille taille;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Taille getTaille() {
+        return taille;
+    }
+
+    public void setTaille(Taille taille) {
+        this.taille = taille;
+    }
+
+    public Dish(String nom, Taille taille) {
+        this.nom = nom;
+        this.taille = taille;
+    }
+
 }
