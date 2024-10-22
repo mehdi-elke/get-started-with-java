0a3800af8ffbabfcf681720f01a9ca92c694aec0
Adrien Degand <adrien.degand@hotmail.com>
2024-10-22 15:32:54 +0200

    Add restaurant

--- src/main/java/fr/baretto/tp2/Restaurant.java
+++ src/main/java/fr/baretto/tp2/Restaurant.java
@@ -1,4 +1,27 @@
 package fr.baretto.tp2;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 public class Restaurant {
+    private String name;
+
+    public Restaurant(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Order prepareOrder(List<Dish> dishes, double price, String deliveryPlace) throws InterruptedException {
+        Thread.sleep(new Random().nextInt(3000));
+        Map<Dish, Integer> dishesMap = new HashMap<>();
+        for(Dish dish : dishes){
+            dishesMap.merge(dish, 1, Integer::sum);
+        }
+        return new Order(this, dishesMap, price, deliveryPlace);
+    }
 }
