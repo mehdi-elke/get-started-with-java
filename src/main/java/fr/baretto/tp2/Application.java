233b076db9bf19bbf1372f320e543d0390ef1a42
Yves-ROland <yvesrolanddebo@gmail.com>
2024-10-22 16:34:31 +0200

    TP2 - Q3

--- src/main/java/fr/baretto/tp2/Application.java
+++ src/main/java/fr/baretto/tp2/Application.java
@@ -3,6 +3,7 @@ package fr.baretto.tp2;
 public class Application {
     public static void main(String[] args) {
         Restaurant restaurant = new Restaurant("Le Wagon");
+        Restaurant restaurant2 = new Restaurant("O'Tacos");
         DeliveryPlateform deliveryPlateform = new DeliveryPlateform();
         Order order1 = restaurant.prepareOrder(new Dish("Tacos", 8.0), 4, 11, "28 rue d'inkermann");
         Order order2 = restaurant.prepareOrder(new Dish("Pizza", 8.0), 1, 15, "81 rue d'inkermann roubaix");
@@ -10,5 +11,8 @@ public class Application {
         deliveryPlateform.deliverOrder(order1);
         deliveryPlateform.deliverOrder(order2);
         deliveryPlateform.deliverOrder(order3);
+
+        deliveryPlateform.subscribe(restaurant);
+        deliveryPlateform.subscribe(restaurant2);
     }
 }
--- src/main/java/fr/baretto/tp2/DeliveryPlateform.java
+++ src/main/java/fr/baretto/tp2/DeliveryPlateform.java
@@ -3,12 +3,19 @@ package fr.baretto.tp2;
 public class DeliveryPlateform {
     public void update(Order order) {
         deliverOrder(order);
+        System.out.println("Nouvelle commande reçue pour livraison : " +
+                order.getDish().getName() + ", Quantité : " + order.getQuantity() +
+                ", Adresse : " + order.getAddress());
     }
 
     public void deliverOrder(Order order) {
         String msg = "Votre commande de " + order.getQuantity() + " " + order.getDish().getName() + " a été livrée à cette adresse: " + order.getAddress();
         msg += " pour un montant de " + order.getInitialPrice() * order.getQuantity() + "€";
         System.out.println(msg);
+    }
+
+    public void subscribe(Restaurant restaurant) {
+        System.out.println("Inscription à la plateforme de livraison du restaurant " + restaurant.getName());
     }
 }
 
