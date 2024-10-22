6382b5ed9526dab59a97d71106dcb34ac846d0a2
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2024-10-22 16:17:03 +0200

    Add observer
    Add observer

--- src/main/java/fr/baretto/tp2/Restaurant.java
+++ src/main/java/fr/baretto/tp2/Restaurant.java
@@ -1,27 +1,40 @@
 package fr.baretto.tp2;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 public class Restaurant {
     private String name;
+    private List<Observer> observers;
 
     public Restaurant(String name) {
         this.name = name;
+        this.observers = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
-    public Order prepareOrder(List<Dish> dishes, double price, String deliveryPlace) throws InterruptedException {
+    public void prepareOrder(List<Dish> dishes, double price, String deliveryPlace) throws InterruptedException {
         Thread.sleep(new Random().nextInt(3000));
         Map<Dish, Integer> dishesMap = new HashMap<>();
         for(Dish dish : dishes){
             dishesMap.merge(dish, 1, Integer::sum);
         }
-        return new Order(this, dishesMap, price, deliveryPlace);
+        this.notifyObservers("finished order", new Order(this, dishesMap, price, deliveryPlace));
+    }
+
+    public void notifyObservers(String event, Order order){
+        this.observers.forEach(observer -> {
+            try {
+                observer.update(event, order);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public void addObserver(Observer observer){
+        this.observers.add(observer);
     }
 }
