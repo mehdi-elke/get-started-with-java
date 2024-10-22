77045f02e98815700d079640a1d39a702ca95c66
Kylian <libraaa201@gmail.com>
2024-10-22 15:45:11 +0200

    Class_Application_WIP

--- src/main/java/fr/baretto/tp2/Application.java
+++ src/main/java/fr/baretto/tp2/Application.java
@@ -1,10 +1,17 @@
 package fr.baretto.tp2;
+import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
-        Restaurant foodFast = new Restaurant();
+    public static void main(String[] args) throws InterruptedException {
+        Restaurant restaurant = new Restaurant("FoodFast");
         DeliveryPlateform foodFastLivraison = new DeliveryPlateform();
+        Dish pates = new Dish("pâtes", DishSize.M);
+        Dish lasagnes = new Dish("lasagnes", DishSize.L);
+        Dish pizza = new Dish("pizza", DishSize.S);
 
+
+
+        restaurant.prepareOrder(List.of(pates, lasagnes, pizza), 30, "Lille");
 
 
     }
