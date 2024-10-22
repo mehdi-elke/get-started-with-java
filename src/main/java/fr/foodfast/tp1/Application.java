d9e6722dde41324f5efbc74f51d0ddd7d4c3178d
Jugurtha <saadajugurtha59@gmail.com>
2024-10-22 15:41:35 +0200

    Init + Add Dish, Order, Restaurant, DeliveryPlatform

--- src/main/java/fr/foodfast/tp1/Application.java
+++ src/main/java/fr/foodfast/tp1/Application.java
@@ -4,6 +4,9 @@ public class Application {
     public static void main(String[] args) {
         DeliveryPlatform livraison = new DeliveryPlatform();
         Restaurant restaurant = new Restaurant("Restaurant Gourmet");
-
+        for (int i = 0; i < 5; i++) {
+            Order order = restaurant.prepareOrder();
+            deliveryPlateform.delivery(order);
+        }
     }
 }
