60b68e7030f58ba41f34ac65a68d6fb4e3930869
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2024-10-22 15:17:57 +0200

    Order Class
    Order Class

--- src/main/java/fr/baretto/tp2/Order.java
+++ src/main/java/fr/baretto/tp2/Order.java
@@ -1,4 +1,17 @@
 package fr.baretto.tp2;
 
+import java.util.Map;
+
 public class Order {
+    private Restaurant restaurant;
+    private Map<Dish, Integer> dishes;
+    private double price;
+    private String deliveryPlace;
+
+    public Order(Restaurant restaurant, Map<Dish, Integer> dishes, double price, String deliveryPlace) {
+        this.restaurant = restaurant;
+        this.dishes = dishes;
+        this.price = price;
+        this.deliveryPlace = deliveryPlace;
+    }
 }
