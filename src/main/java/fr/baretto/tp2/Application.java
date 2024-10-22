619740b9e807296763a6c7060167df1d60fa4025
MehdiTrari <trarimehdi59@gmail.com>
2024-10-22 16:43:21 +0200

    mehdi yves theo

--- src/main/java/fr/baretto/tp2/Application.java
+++ src/main/java/fr/baretto/tp2/Application.java
@@ -2,17 +2,19 @@ package fr.baretto.tp2;
 
 public class Application {
     public static void main(String[] args) {
-        Restaurant restaurant = new Restaurant("Le Wagon");
+        Restaurant restaurant1 = new Restaurant("Le Wagon");
         Restaurant restaurant2 = new Restaurant("O'Tacos");
-        DeliveryPlateform deliveryPlateform = new DeliveryPlateform();
-        Order order1 = restaurant.prepareOrder(new Dish("Tacos", 8.0), 4, 11, "28 rue d'inkermann");
-        Order order2 = restaurant.prepareOrder(new Dish("Pizza", 8.0), 1, 15, "81 rue d'inkermann roubaix");
-        Order order3 = restaurant.prepareOrder(new Dish("Burger", 8.0), 10, 5.5, "43 rue du jardin des plantes");
-        deliveryPlateform.deliverOrder(order1);
-        deliveryPlateform.deliverOrder(order2);
-        deliveryPlateform.deliverOrder(order3);
 
-        deliveryPlateform.subscribe(restaurant);
+        DeliveryPlateform deliveryPlateform = new DeliveryPlateform();
+
+        restaurant1.addObserver(deliveryPlateform);
+        restaurant2.addObserver(deliveryPlateform);
+
+        Order order1 = restaurant1.prepareOrder(new Dish("Tacos", 8.0), 4, 11.0, "28 rue d'inkermann");
+        Order order2 = restaurant1.prepareOrder(new Dish("Pizza", 8.0), 1, 15.0, "81 rue d'inkermann, Roubaix");
+        Order order3 = restaurant2.prepareOrder(new Dish("Burger", 5.5), 10, 5.5, "43 rue du jardin des plantes");
+
+        deliveryPlateform.subscribe(restaurant1);
         deliveryPlateform.subscribe(restaurant2);
     }
 }
