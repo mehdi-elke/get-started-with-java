f13a5c2fe80d92e7ba2255094f0282a0c8ededf4
MehdiTrari <trarimehdi59@gmail.com>
2024-11-19 15:28:16 +0100

    ajout du uniqueCommand

--- src/main/java/fr/baretto/tp2/DeliveryPlateform.java
+++ src/main/java/fr/baretto/tp2/DeliveryPlateform.java
@@ -1,15 +1,30 @@
 package fr.baretto.tp2;
 
-public class DeliveryPlateform {
+import java.util.HashSet;
+import java.util.Set;
+
+public class DeliveryPlatform {
+    private Set<Order> uniqueOrders;
+
+    public DeliveryPlatform() {
+        uniqueOrders = new HashSet<>();
+    }
+
     public void update(Order order) {
-        deliverOrder(order);
-        System.out.println("Nouvelle commande reçue pour livraison : " +
-                order.getDish().getName() + ", Quantité : " + order.getQuantity() +
-                ", Adresse : " + order.getAddress());
+        if (uniqueOrders.add(order)) {
+            deliverOrder(order);
+            System.out.println("Nouvelle commande reçue pour livraison : " +
+                    order.getDish().getName() + ", Quantité : " + order.getQuantity() +
+                    ", Adresse : " + order.getAddress());
+        } else {
+            System.out.println("Commande déjà existante et ignorée : " + order.getDish().getName() +
+                    ", Quantité : " + order.getQuantity() + ", Adresse : " + order.getAddress());
+        }
     }
 
     public void deliverOrder(Order order) {
-        String msg = "Votre commande de " + order.getQuantity() + " " + order.getDish().getName() + " a été livrée à cette adresse: " + order.getAddress();
+        String msg = "Votre commande de " + order.getQuantity() + " " + order.getDish().getName() +
+                " a été livrée à cette adresse: " + order.getAddress();
         msg += " pour un montant de " + order.getInitialPrice() * order.getQuantity() + "€";
         System.out.println(msg);
     }
@@ -17,5 +32,12 @@ public class DeliveryPlateform {
     public void subscribe(Restaurant restaurant) {
         System.out.println("Inscription à la plateforme de livraison du restaurant " + restaurant.getName());
     }
-}
 
+    public int getOrderCount() {
+        return uniqueOrders.size();
+    }
+
+    public boolean containsOrder(Order order) {
+        return uniqueOrders.contains(order);
+    }
+}
--- src/test/java/fr/baretto/tp2/DeliveryPlateformTest.java
+++ src/test/java/fr/baretto/tp2/DeliveryPlateformTest.java
@@ -1,8 +1,20 @@
 package fr.baretto.tp2;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DeliveryPlateformTest {
+
+    private DeliveryPlatform deliveryPlatform;
+    private Restaurant restaurant;
+
+    @BeforeEach
+    public void setup() {
+        deliveryPlatform = new DeliveryPlatform();
+        restaurant = new Restaurant("Testaurant");
+        restaurant.addObserver(deliveryPlatform);
+    }
 
     @Test
     public void testDeliverOrder() {
@@ -12,5 +24,40 @@ public class DeliveryPlateformTest {
         DeliveryPlateform platform = new DeliveryPlateform();
 
         platform.deliverOrder(order);
+    }
+
+    @Test
+    public void testUniqueOrder() {
+        Order order1 = new Order(restaurant, new Dish("Pizza", 8.0), 2, 11.0, "10 rue de la paix");
+
+        deliveryPlatform.update(order1);
+        assertTrue(deliveryPlatform.containsOrder(order1));
+        assertEquals(1, deliveryPlatform.getOrderCount());
+    }
+
+    @Test
+    public void testDuplicateOrder() {
+        Order order1 = new Order(restaurant, new Dish("Pizza", 8.0), 2, 11.0, "10 rue de la paix");
+        Order duplicateOrder = new Order(restaurant, new Dish("Pizza", 8.0), 2, 11.0, "10 rue de la paix");
+
+        deliveryPlatform.update(order1);
+
+        deliveryPlatform.update(duplicateOrder);
+
+        assertEquals(1, deliveryPlatform.getOrderCount());
+        assertTrue(deliveryPlatform.containsOrder(order1));
+    }
+
+    @Test
+    public void testDifferentOrder() {
+        Order order1 = new Order(restaurant, new Dish("Pizza", 8.0), 2, 11.0, "10 rue de la paix");
+        Order order2 = new Order(restaurant, new Dish("Tacos", 5.0), 1, 15.0, "20 rue de la liberté");
+
+        deliveryPlatform.update(order1);
+        deliveryPlatform.update(order2);
+
+        assertEquals(2, deliveryPlatform.getOrderCount());
+        assertTrue(deliveryPlatform.containsOrder(order1));
+        assertTrue(deliveryPlatform.containsOrder(order2));
     }
 }
