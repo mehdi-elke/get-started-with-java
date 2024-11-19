a5e0ac1b13b2bdbc11eab2d8a8d58715ebac86ab
Zaouich123 <etienne.baillie@gmail.com>
2024-11-19 15:17:15 +0100

    TP3 question 3

--- src/main/java/tp2/Application.java
+++ src/main/java/tp2/Application.java
@@ -2,24 +2,46 @@ package tp2;
 
 import java.util.HashMap;
 
-
-public class  Application {
+public class Application {
 
     public static void main(String[] args) {
+
         DeliveryPlatform uberEat = new DeliveryPlatform();
+
+
         Restaurant McDo = new Restaurant("McDo");
+        Restaurant KFC = new Restaurant("KFC");
 
 
-        McDo.addObserver(uberEat);
+        uberEat.subscribeRestaurant(McDo);
+        uberEat.subscribeRestaurant(KFC);
+
 
         Dish BigMac = new Dish("BigMac", Dish.Taille.S);
-        Dish Fries = new Dish("McFries", Dish.Taille.M);
-
-        HashMap<Dish, Integer> Order1 = new HashMap<>();
-        Order1.put(BigMac, 3);
-        Order1.put(Fries, 2);
+        Dish Fries = new Dish("Fries", Dish.Taille.M);
+        Dish Zinger = new Dish("Zinger", Dish.Taille.L);
 
 
-        McDo.prepareOrder(Order1, 25.65, "45 Rue du port");
+        HashMap<Dish, Integer> McDoOrder = new HashMap<>();
+        McDoOrder.put(BigMac, 2);
+        McDoOrder.put(Fries, 3);
+
+
+        HashMap<Dish, Integer> KFCOrder = new HashMap<>();
+        KFCOrder.put(Zinger, 1);
+
+
+        new Thread(() -> McDo.prepareOrder(McDoOrder, 25.65, "45 Rue du port")).start();
+        new Thread(() -> KFC.prepareOrder(KFCOrder, 18.50, "12 Avenue des Champs")).start();
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        uberEat.shutdown();
     }
 }
--- src/main/java/tp2/DeliveryPlatform.java
+++ src/main/java/tp2/DeliveryPlatform.java
@@ -1,14 +1,45 @@
 package tp2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class DeliveryPlatform implements Observer {
+
+    private List<Restaurant> subscribedRestaurants = new ArrayList<>();
+    private ExecutorService executorService;
+
+    public DeliveryPlatform() {
+
+        this.executorService = Executors.newFixedThreadPool(4);
+    }
+
+    public void subscribeRestaurant(Restaurant restaurant) {
+        subscribedRestaurants.add(restaurant);
+        restaurant.addObserver(this);
+    }
+
+    public void unsubscribeRestaurant(Restaurant restaurant) {
+        subscribedRestaurants.remove(restaurant);
+        restaurant.removeObserver(this);
+    }
 
     @Override
     public void update(Order order) {
-        System.out.println("Notification received: A new order has been prepared: " + order);
-        delivery(order);
+
+        executorService.submit(() -> {
+            System.out.println("Processing order from " + order.getRestaurant().getName());
+            delivery(order);
+        });
     }
 
     public void delivery(Order order) {
         System.out.println("La commande est livrée: " + order);
+    }
+
+    public void shutdown() {
+        executorService.shutdown();
+        System.out.println("Delivery platform shutting down.");
     }
 }
